7201e2b877b53760c9a22c5e8c097d48ba9c1164
yago <ybarbosa1608@gmail.com>
2026-07-28 19:22:00 -0300

    adicionado testes para criação de locais com validação de sucesso e falha

--- src/test/java/br/com/yagobarbosa/placeservice/PlaceServiceApplicationTests.java
+++ src/test/java/br/com/yagobarbosa/placeservice/PlaceServiceApplicationTests.java
@@ -1,13 +1,59 @@
 package br.com.yagobarbosa.placeservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.yagobarbosa.placeservice.api.PlaceRequest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PlaceServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @LocalServerPort
+  private int port;
 
+  private WebTestClient webTestClient;
+
+  @BeforeEach
+  public void setUp() {
+    this.webTestClient = WebTestClient.bindToServer()
+        .baseUrl("http://localhost:" + port)
+        .build();
+  }
+
+  @Test
+  public void testCreatePlaceSucess() {
+    var name = "Valid Name";
+    var state = "Valid state";
+    var slug = "valid-name";
+
+    webTestClient
+        .post()
+        .uri("/places")
+        .bodyValue(new PlaceRequest(name, state))
+        .exchange()
+        .expectStatus().isCreated()
+        .expectBody()
+        .jsonPath("$.name").isEqualTo(name)
+        .jsonPath("$.state").isEqualTo(state)
+        .jsonPath("$.slug").isEqualTo(slug)
+        .jsonPath("$.createdAt").isNotEmpty()
+        .jsonPath("$.updatedAt").isNotEmpty();
+  }
+
+  @Test
+  public void testCreatePlaceFailure() {
+    var name = "";
+    var state = "";
+
+    webTestClient
+        .post()
+        .uri("/places")
+        .bodyValue(new PlaceRequest(name, state))
+        .exchange()
+        .expectStatus()
+        .isBadRequest();
+  }
 }
